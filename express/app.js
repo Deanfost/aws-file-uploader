dffade7ebc97afda496b2fd8590f71dcaf66cc4b
Dean Foster <Deanfoster45@gmail.com>
2021-09-16 11:56:25 -0400

    Add S3 SDK and dotenv, configure env vars

--- express/app.js
+++ express/app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var fileRouter = require('./routes/files');
+let fileRouter = require('./routes/files');
 
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- express/routes/files.js
+++ express/routes/files.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let S3 = require('@aws-sdk/client-s3');
+let router = express.Router();
+let s3Client = new S3.S3Client({region: process.env.REGION});
 
 /* GET uploaded files */
 router.get('/', function(req, res) {
-    res.sendStatus(501);
+    res.send('region: ' + process.env.REGION);
 });
 
 /* POST a new file */
