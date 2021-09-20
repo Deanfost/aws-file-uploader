7411b3601e06c3cf12046d30f5ecffc13c018dee
Dean Foster <Deanfoster45@gmail.com>
2021-09-19 20:50:15 -0400

    Work on object list endpoint

--- express/app.js
+++ express/app.js
@@ -13,6 +13,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', fileRouter);
+
+// Error handler
+app.use(function(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+    // Log someting
+    console.error(err.stack);
+    res.status(500);
+    res.send('Something broke!');
+});
 
 module.exports = app;
--- express/routes/files.js
+++ express/routes/files.js
@@ -3,20 +3,56 @@ let S3 = require('@aws-sdk/client-s3');
 let router = express.Router();
 let s3Client = new S3.S3Client({region: process.env.REGION});
 
-/* GET uploaded files */
-router.get('/', function(req, res) {
-    res.send('env ' + process.env.REGION);
-    // res.send('region: ' + process.env.REGION);
+/* GET objects list at prefix */
+router.get('/', acceptQueryParams(['prefix']), async function(req, res, next) {
+    let prefix = req.query.prefix ? req.query.prefix : '.';
+
+    // Configure S3 command
+    const bucketParams = {
+        Bucket: process.env.BUCKET,
+        prefix
+    };
+
+    try {
+        // Request object list from S3 and send
+        const data = await s3Client.send(new S3.ListObjectsCommand(bucketParams));
+        res.send(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+/* GET uploaded objects at prefix */
+router.get('/objects', acceptQueryParams([]), async function(req, res, next) {
+
 });
 
 /* POST a new file */
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
     res.sendStatus(501);
 });
 
 /* DELETE a file */
-router.delete('/', function(req, res) {
+router.delete('/', async function(req, res) {
     res.sendStatus(501);
 });
+
+
+/* Middleware */
+
+// Only allow these query params to be present
+function acceptQueryParams(params) {
+    return (req, res, next) => {
+        // Throw 400 if invalid query params
+        let queries = req.query;
+        for (q in Object.keys(queries)) {
+            if (!params.includes(q)) {
+                res.status(400);
+                res.send('Malformed request, check query params');
+            }
+        }
+        next();
+    };
+};
 
 module.exports = router;
